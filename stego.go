9bd848f8aeb59ab477ebf8a7a873ec52bfda1fa6
Fredrik Åhs <freahs@kth.se>
2017-08-29 20:18:03 +0200

    added a default scrambler

--- stego.go
+++ stego.go
@@ -20,6 +20,47 @@ const (
 	headerSize int  = 6 // Bytes in header
 )
 
+// stegoImage wraps the underlying byte array of an image and provides convenience methods
+// for reading and writing bits to
+type stegoImage struct {
+	pix       []uint8
+	pixOffset func(x, y int) int
+	step      int
+}
+
+func (s *stegoImage) pos(x, y, c int) int {
+	return s.pixOffset(x, y) + c*s.step
+}
+
+func (s *stegoImage) Read(x, y, c int) bool {
+	p := s.pos(x, y, c)
+	return s.pix[p]&0x01 == 1
+}
+
+func (s *stegoImage) Enable(x, y, c int) {
+	p := s.pos(x, y, c)
+	s.pix[p] = s.pix[p] | 0x01 // LSB = 1
+}
+
+func (s *stegoImage) Disable(x, y, c int) {
+	p := s.pos(x, y, c)
+	s.pix[p] = s.pix[p] & 0xFE // LSB = 0
+}
+
+func newstegoImage(img image.Image) (*stegoImage, error) {
+	switch i := img.(type) {
+	case *image.RGBA:
+		return &stegoImage{i.Pix, i.PixOffset, 1}, nil
+	case *image.RGBA64:
+		return &stegoImage{i.Pix, i.PixOffset, 2}, nil
+	case *image.NRGBA:
+		return &stegoImage{i.Pix, i.PixOffset, 1}, nil
+	case *image.NRGBA64:
+		return &stegoImage{i.Pix, i.PixOffset, 2}, nil
+	}
+	return nil, StegoError("Image format not supported")
+}
+
 // Scrambler is used to distribute data in an image, but an implementation should not alter the
 // image itself.
 //
@@ -38,12 +79,41 @@ type Scrambler interface {
 	Cap() int
 }
 
+type DefaultScrambler struct {
+	i, w, h    int
+	chans, cap int
+}
+
+func (s *DefaultScrambler) Init(image image.Image) {
+	bounds := image.Bounds()
+	s.i = 0
+	s.w = bounds.Max.X - bounds.Min.X
+	s.h = bounds.Max.Y - bounds.Min.Y
+	s.chans = 3
+	s.cap = s.w * s.h * s.chans
+}
+
+func (s *DefaultScrambler) Next() (x, y, c int) {
+	x = (s.i / s.chans) % s.w
+	y = s.i / (s.chans * s.w)
+	c = s.i % s.chans
+	s.i++
+	return
+}
+
+func (s *DefaultScrambler) Cap() int {
+	return s.cap
+}
+
 // Encode encodes a slice of Bytes in the Image using the Scrambler. If it fails for some reason,
 // an error is returned.
 func Encode(bytes []byte, img image.Image, scrambler Scrambler) (image.Image, error) {
 
-	source := img.(*image.RGBA)
-	scrambler.Init(source)
+	w, err := newstegoImage(img)
+	if err != nil {
+		return nil, err
+	}
+	scrambler.Init(img)
 
 	// Make a slice of header and argument bytes, to encode in the image.
 	data := make([]byte, headerSize)
@@ -58,24 +128,25 @@ func Encode(bytes []byte, img image.Image, scrambler Scrambler) (image.Image, er
 	}
 
 	for _, b := range data {
-		writeByte(b, source, scrambler)
+		writeByte(b, w, scrambler)
 	}
 
-	output := image.NewRGBA(source.Bounds())
-	output.Pix = source.Pix
-	return output, nil
+	return img, nil
 }
 
 // Decode reads bytes from a Image using a Scrambler. If it fails for some reason, an error is
 // returned
 func Decode(img image.Image, scrambler Scrambler) ([]byte, error) {
-	source := img.(*image.RGBA)
-	scrambler.Init(source)
+	w, err := newstegoImage(img)
+	if err != nil {
+		return nil, err
+	}
+	scrambler.Init(img)
 
 	// Read the header...
 	header := make([]byte, headerSize)
 	for i := 0; i < len(header); i++ {
-		header[i] = readByte(source, scrambler)
+		header[i] = readByte(w, scrambler)
 
 	}
 
@@ -93,18 +164,17 @@ func Decode(img image.Image, scrambler Scrambler) ([]byte, error) {
 	// Read as many bytes as specified in the header and return the result.
 	ret := make([]byte, size)
 	for i := 0; i < size; i++ {
-		ret[i] = readByte(source, scrambler)
+		ret[i] = readByte(w, scrambler)
 	}
 	return ret, nil
 }
 
 // readByte reads a byte from a image using a scrambler.
-func readByte(img *image.RGBA, scrambler Scrambler) (b byte) {
+func readByte(w *stegoImage, scrambler Scrambler) (b byte) {
 	var i uint8
 	for i = 0; i < 8; i++ {
 		x, y, c := scrambler.Next() // Next pixel coords and channel.
-		p := img.PixOffset(x, y) + c
-		if img.Pix[p]&0x01 == 1 { // Read current bit.
+		if w.Read(x, y, c) {        // Read current bit.
 			b += (1 << i) // Increase byte if necessary.
 		}
 	}
@@ -112,16 +182,14 @@ func readByte(img *image.RGBA, scrambler Scrambler) (b byte) {
 }
 
 // writeByte writes a byte to a image using a scrambler.
-func writeByte(b byte, img *image.RGBA, scrambler Scrambler) {
+func writeByte(b byte, w *stegoImage, scrambler Scrambler) {
 	var i uint8
 	for i = 0; i < 8; i++ { // Step through each bit
-		bit := (b>>i)&0x01 == 1
 		x, y, c := scrambler.Next()
-		p := img.PixOffset(x, y) + c
-		if bit { // Set the bit.
-			img.Pix[p] = img.Pix[p] | 0x01 // LSB = 1
+		if (b>>i)&0x01 == 1 { // Set the bit.
+			w.Enable(x, y, c)
 		} else {
-			img.Pix[p] = img.Pix[p] & 0xFE // LSB = 0
+			w.Disable(x, y, c)
 		}
 	}
 }
